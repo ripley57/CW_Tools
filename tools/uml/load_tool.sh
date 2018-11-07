b46d11c34f3e85e015ef7b34ef47d6a5ae91452a
Jeremy <ripley57@users.noreply.github.com>
2018-11-07 13:38:50 +0000

    Update load_tool.sh
    Tested the changes for uml command.

--- tools/uml/load_tool.sh
+++ tools/uml/load_tool.sh
@@ -10,11 +10,11 @@ function uml() {
 	if [ "$1" = '-h' ]; then
         	usage uml
         	return
-    	fi
+	fi
 	
 	local umlet_download="https://www.dropbox.com/s/4840hgj2ljjobkk/umlet.zip?dl=1"
 	local umlet_zip="umlet.zip"
-	local umlet_exe="$TOOLS_DIR/uml/umlet-standalone-14.3.0/Umlet/Umlet.exe"
+	local umlet_exe="$TOOLS_DIR/uml/Umlet/Umlet.exe"
 
 	# Delete the existing zip download if the file size was zero bytes, as
 	# this was probably a failed download due to lack of Internet connection.
@@ -22,14 +22,16 @@ function uml() {
 	
 	# Download.
 	if [ ! -f "$TOOLS_DIR/uml/$umlet_zip" ]; then
-      	      (cd "$TOOLS_DIR/uml" && download_file "$umlet_download" "$umlet_zip")
-	fi
-		
-	# Extract.
-	if [ ! -f "$umlet_exe" ]; then
-              (cd "$TOOLS_DIR/uml" && unzip "$umlet_zip" && chmod ugo+rx "$umlet_exe")
+		echo "Downloading $umlet_download ..."
+		(cd "$TOOLS_DIR/uml" && download_file "$umlet_download" "$umlet_zip")
 	fi
 	
+	# Extract.
+	if [ ! -f "$umlet_exe" ]; then
+		echo "Extracting $umlet_zip ..."
+		(cd "$TOOLS_DIR/uml" && unzip "$umlet_zip" && chmod ugo+rx "$umlet_exe")
+	fi
+
 	# Launch.
-    	"$umlet_exe" &
+	"$umlet_exe" &
 }
