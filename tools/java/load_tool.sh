c2e758852ac6fed816c7c2d4bf1e9f55fe1845da
Jeremy <ripley57@users.noreply.github.com>
2018-12-17 09:09:54 +0000

    Update load_tool.sh
    Add javasetup command to pick from an installed Java from Cygwin.

--- tools/java/load_tool.sh
+++ tools/java/load_tool.sh
@@ -9,8 +9,7 @@ TOOLS_DIR=$*
 #    javasetup [search]
 #
 # Where:
-#    search  -  Search for installed java versions and persist
-#               the list.
+#    search  -  Search for installed java versions and persist the list.
 #
 function javasetup() {
     if [ "$1" = '-h' ]; then
@@ -19,22 +18,84 @@ function javasetup() {
     fi
 
     if [ "$1" == "search" ]; then
-        echo "Searching for java JDK installations..."
-		find /cygdrive/c/ -iname "*javac.exe" > "${TMP}/.javasetup" 2>/dev/null
-	
+        echo "Searching for JDK installations (THIS COULD TAKE SEVERAL MINUTES) ..."
+        find /cygdrive/c/ -iname "javac.exe" > "${TMP}/.javasetup" 2>/dev/null
         if [ ! -f "${TMP}/.javasetup" ]; then
-            echo "javasetup: Error searching for Java JDK installations."
+            echo "javasetup: ERROR: Error searching for Java JDK installations."
             return
         fi
 
-		# Read the java installations list into an array.
-		local _java_installs
-		local _i
+        # Read the java installations list into an array.
+        local _java_installs
+        local _i
         IFS=$'\n' _java_installs=( `cat "${TMP}/.javasetup" | sort`)
-        for _i in "${_java_installs[@]}"; do
-            echo $_i
+	
+        # Remove the trailing '/bin/javac.exe' and persist the java dirs.
+        rm -f "${TMP}/.javasetup_list"
+        local _java_dirs
+        declare -a _java_dirs=( "${_java_installs[@]/\/bin\/javac.exe/}" )
+        for _i in "${_java_dirs[@]}"; do
+            echo $_i >> "${TMP}/.javasetup_list"
         done
+	
+        # Build a menu of the java dirs to pick from.
+        rm -f "${TMP}/.javasetup_menu"
+        let _n=1
+        local _javadir
+        while read _javadir; do
+            echo "${_n}) $_javadir" >> "${TMP}/.javasetup_menu"
+            let _n=_n+1
+        done < "${TMP}/.javasetup_list"
+    fi
+		
+    if [ ! -f "${TMP}/.javasetup_menu" ]; then
+        echo "INFO: Please run the \"search\" option to search for installed Javas".
+        return
+    fi
+    
+    # Display the menu to allow the user to select a java version.
+    local _ans
+    local _dir
+    while [ 1 ]
+    do
+        _ans=""
+        _dir=""
+        clearscreen
+	
+        if [ ! -z "$JAVA_HOME" ]; then
+            echo 
+            echo "JAVA_HOME is currently set to:"
+            echo "$JAVA_HOME"
+            echo
 	fi
+	
+        # Display the menu of Javas to select from.
+        echo
+        cat "${TMP}/.javasetup_menu"
+        echo
+	
+        # Ask the user to select a Java and configure the env.
+        echo -n "Please select a Java installation: "
+        read _ans
+        if [[ "$_ans" =~ ^[0-9]+$ ]]; then 
+            # Extract the Java dir.
+            _dir=$(sed -n ${_ans}p "${TMP}/.javasetup_menu" | sed 's#^[0-9][^/]*##')
+		
+            # Configure new Java env.
+            echo
+            echo "Updating env to use: \"${_dir}\" ..."
+            export JAVA_HOME="${_dir}"
+            export PATH="${JAVA_HOME}/bin:$PATH"
+            echo
+            java -version
+            echo
+            echo "JAVA_HOME=$JAVA_HOME"
+            return
+        fi
+
+        echo -n "Bad choice. Press Enter to select again: "
+        read _dummy
+    done
 }
 
 # Description:
