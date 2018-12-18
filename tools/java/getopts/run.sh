f07a1f4e688bfe1fa7341eae6f3c165064e4258c
Jeremy <ripley57@users.noreply.github.com>
2018-12-18 14:46:35 +0000

    Update run.sh

--- tools/java/getopts/run.sh
+++ tools/java/getopts/run.sh
@@ -1,6 +1,20 @@
 # Examples:
 
-#java -jar getoptdemo.jar -a
-#java -jar getoptdemo.jar -d
+java -jar getoptdemo.jar -a
+
 java -jar getoptdemo.jar -b hello
-#java -jar getoptdemo.jar -c goodbye
+
+java -jar getoptdemo.jar --help
+java -jar getoptdemo.jar -help
+java -jar getoptdemo.jar -h
+
+java -jar getoptdemo.jar --outputdir xxx
+java -jar getoptdemo.jar -outputdir yyy
+java -jar getoptdemo.jar -o zzz
+
+java -jar getoptdemo.jar --maximum
+java -jar getoptdemo.jar --maximum=1
+java -jar getoptdemo.jar -maximum
+java -jar getoptdemo.jar -maximum=2
+java -jar getoptdemo.jar -m
+java -jar getoptdemo.jar -m=3
