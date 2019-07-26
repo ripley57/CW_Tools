ec96a8b7f56392dd646a612e17e4021e10592a1a
Jeremy Clough <clough.jeremy@gmail.com>
2019-07-26 19:16:19 +0100

    Rewrite install_nodejs to work on Linux. Add NodeJS http-servlet function wrapper.

--- tools/javascript/load_tool.sh
+++ tools/javascript/load_tool.sh
@@ -1,26 +1,28 @@
 TOOLS_DIR=$*
 
+
 # Description:
-#   Download and install a pre-installed Node.js environment, which 
-#   was created# by the install_nodejs function. That function takes
-#   several minutes to complete, so this should be a quicker way to get 
-#   a working Node.js environment.
+#   Wrapper function for the NodeJS "http-server" command.
+#
+#   Without any argument, this will start a web server 
+#   serving the files in the current directory.
+#
+#   This command will download and install nodejs, if not
+#   done already, into CW_Tools/tools/javascript/nodejs.
 #
 # Usage:
-#   install_nodejs_preinstalled
+#   http-server [dir-of-files-to-serve]
 #
-function install_nodejs_preinstalled() {
+function http-server() {
     if [ "$1" = '-h' ]; then
-        usage install_nodejs_preinstalled
+        usage http-server
         return
     fi
 
-    download_file "$DOWNLOAD_NODEJS_PREINSTALLED" "node.zip"
-    unzip node.zip
-    find ./node -iname node.exe -o -iname npm.cmd -exec chmod +x {} \;
+    [ ! -f "$TOOLS_DIR/javascript/nodejs/node_modules/http-server/bin/http-server" ] && install_nodejs
 
-    echo
-    echo You might now want to run "run_nodejs_demos"
+    printf "\nLaunching CW_Tools http-server wrapper ...\n"
+    "$TOOLS_DIR/javascript/nodejs//node_modules/http-server/bin/http-server" $*
 }
 
 # Description:
@@ -82,89 +84,92 @@ EOI
 #            cmd /c .\\node.exe demo.js
 #
 # Usage:
-#   install_nodejs <dirname>
+#   install_nodejs [<dirname>]
 #
 # Example usage:
 #   mkdir tmp1
 #   install_nodejs tmp1
 #
 function install_nodejs() {
-    	if [ "$1" = '-h' -o $# -ne 1 ]; then
+    	if [ "$1" = '-h' ]; then
         	usage install_nodejs
         	return
     	fi
 	
-	local _dirname="$(cygpath -aw "$1")"
-	
-	if [ -f "$_dirname" ]; then
-		echo "ERROR: Directory already exists: $_dirname. Aborting..."
-		return
-	fi
-	
-	mkdir -p "$_dirname" || ( \
-		echo "ERROR: Failed to created directory: $_dirname. Aborting..."
-		return
-	)
-	local _pwd=$(PWD)
+	local _dirname="$(_cygpath -aw "$1")"
+        [ -z "$_dirname" ] && _dirname="$TOOLS_DIR/javascript/nodejs/"
+	mkdir -p "$_dirname" || (echo "ERROR: Failed to create directory: $_dirname. Aborting..." && return)
+
+	local _pwd=$PWD
 	cd "$_dirname"
 	
 	echo "Downloading Node.js ..."
-    	download_file "https://nodejs.org/dist/v8.11.3/node-v8.11.3-win-x64.zip" "nodejs.zip"
-	if [ ! -f "nodejs.zip" ]; then
-		echo "ERROR: Failed to download Node.js. Aborting..."
-		cd "$_pwd"
-		return
-	fi
-	
-	echo "Downloading npm ..."
-	download_file "https://github.com/npm/npm/archive/v6.1.0.zip" "npm.zip"
-	if [ ! -f "npm.zip" ]; then
-		echo "ERROR: Failed to download npm.zip. Aborting..."
-		cd "$_pwd"
-		return
-	fi
-	
-	echo "Extracting node.exe from nodejs.zip ..."
-	/usr/bin/unzip nodejs.zip node-v8.11.3-win-x64/node.exe 2>&1 >/dev/null
-	if [ ! -f "node-v8.11.3-win-x64/node.exe" ]; then
-		echo "ERROR: Failed to extract node.exe from nodejs.zip. Aborting..."
-		cd "$_pwd"
-		return
-	fi
-	cp node-v8.11.3-win-x64/node.exe .
-	# Tidy up.
-	rm -fr node-v8.11.3-win-x64
-	rm -f nodejs.zip
-	
-	echo "Creating node_modules folder ..."
-	mkdir -p node_modules
-	echo "Extracting npm.zip into node_modules/ ..."
-	/usr/bin/unzip -q npm.zip -d node_modules/ 2>&1 >/dev/null
-	if [ ! -d "node_modules/npm-6.1.0" ]; then
-		echo "ERROR: Failed to extract npm.zip. Aborting..."
-		cd "$_pwd"
-		return
-	fi
-	# Rename the extracted folder.
-	mv node_modules/npm-6.1.0 node_modules/npm
-	# Tidy up.
-	rm -f npm.zip
-	echo "Copying npm.cmd to current directory..."
-	cp node_modules/npm/bin/npm.cmd .
-	
-	# Run npm installers.
-	chmod +x npm.cmd node.exe
-	cmd /c .\\npm.cmd install -g express
-	cmd /c .\\npm.cmd install express --save
-	
-	# Install some extra modules that are useful.
-	cmd /c .\\npm.cmd i --save csvtojson
-	cmd /c .\\npm.cmd install http-server -g
+	if isLinux ; then
+		if [ ! -f node ]; then
+			download_file "https://nodejs.org/dist/v8.11.3/node-v8.11.3-linux-x64.tar.gz" "nodejs.tar.gz"
+			[ ! -f "nodejs.tar.gz" ] && echo "ERROR: Failed to download nodejs.tar.gz. Aborting..." && cd "$_pwd" && return
 
-	run_nodejs_demos
+			echo "Extracting node from nodejs.tar.gz ..."
+			tar xvzf nodejs.tar.gz node-v8.11.3-linux-x64/bin/node 2>&1 >/dev/null
+			[ ! -f "node-v8.11.3-linux-x64/bin/node" ] && echo "ERROR: Failed to extract node from nodejs.tar.gz. Aborting..." && cd "$_pwd" && return
+			cp node-v8.11.3-linux-x64/bin/node .
+		fi
+	else
+		if [ ! -f node.exe ]; then
+    			download_file "https://nodejs.org/dist/v8.11.3/node-v8.11.3-win-x64.zip" "nodejs.zip"
+			[ ! -f "nodejs.zip" ] && echo "ERROR: Failed to download nodejs.zip. Aborting..." && cd "$_pwd" && return
+
+			echo "Extracting node.exe from nodejs.zip ..."
+			/usr/bin/unzip nodejs.zip node-v8.11.3-win-x64/node.exe 2>&1 >/dev/null
+			[ ! -f "node-v8.11.3-win-x64/node.exe" ] && echo "ERROR: Failed to extract node.exe from nodejs.zip. Aborting..." && cd "$_pwd" && return
+			cp node-v8.11.3-win-x64/node.exe .
+		fi
+	fi
+
+	if [ ! -d node_modules/npm ]; then
+		echo "Downloading npm.zip ..."
+		download_file "https://github.com/npm/npm/archive/v6.1.0.zip" "npm.zip"
+		[ ! -f "npm.zip" ] && echo "ERROR: Failed to download npm.zip. Aborting..." && cd "$_pwd" && return
 	
-	echo
-	echo "Node.js installation complete!"
+		echo "Extracting npm.zip into node_modules/ ..."
+		mkdir -p node_modules
+		/usr/bin/unzip -q npm.zip -d node_modules/ 2>&1 >/dev/null
+		[ ! -d "node_modules/npm-6.1.0" ] && echo "ERROR: Failed to extract npm.zip. Aborting..." && cd "$_pwd" && return
+		mv node_modules/npm-6.1.0 node_modules/npm
+
+		if isLinux ; then
+			echo "Copying npm to current directory ..."
+			cp node_modules/npm/bin/npm .
+		else
+			echo "Copying npm.cmd to current directory ..."
+			cp node_modules/npm/bin/npm.cmd .
+		fi
+	
+		# Run npm to install some stuff.
+		# Note: We don't use the npm install 'global' (-g) option, because that will
+		#       copy the exe (e.g. bin/http-server) to somewhere like /usr/local/bin.
+		if isLinux ; then
+			chmod +x npm node
+			# Express (https://www.tutorialkart.com/nodejs/what-is-express-js/)
+			#./npm --silent install express
+			# csvtojson (https://www.npmjs.com/package/csvtojson)
+			./npm --silent install csvtojson
+			# http-server (https://www.js-tutorials.com/nodejs-tutorial/setup-node-http-server-example/)
+			./npm --silent install http-server
+		else
+			chmod +x npm.cmd node.exe
+			# Express (https://www.tutorialkart.com/nodejs/what-is-express-js/)
+			#cmd /c .\\npm.cmd --silent install express
+			# csvtojson (https://www.npmjs.com/package/csvtojson)
+			cmd /c .\\npm.cmd --silent install csvtojson
+			# http-server (https://www.js-tutorials.com/nodejs-tutorial/setup-node-http-server-example/)
+			cmd /c .\\npm.cmd --silent install http-server
+		fi
+		printf "\nYou may now want to run some NodeJS demos:\nrun_nodejs_demos\n\n"
+	fi
+
+	cd "$_pwd"
+	printf  "\nNode.js installation complete!"
 }
 
 
