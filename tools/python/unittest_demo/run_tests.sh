5bdbee2f710cbb0de9873f08a8a02adf9651c410
Jeremy Clough <clough.jeremy@gmail.com>
2019-08-17 19:49:44 +0100

    Add useful link re TDD with Python unittests

--- tools/python/unittest_demo/run_tests.sh
+++ tools/python/unittest_demo/run_tests.sh
@@ -1,4 +1,7 @@
-#  See: https://docs.python.org/3.5/library/unittest.html#unittest-test-discovery
+#  See: 
+#	https://docs.python.org/3.5/library/unittest.html#unittest-test-discovery
+#	https://code.tutsplus.com/tutorials/beginning-test-driven-development-in-python--net-30137
+
 
 # Discover all test modules automatically in the local directory (and sub-dirs)
 # Test module files named "test*.py" are searched for automatically by default.
