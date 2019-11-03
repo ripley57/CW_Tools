8650728f3bbaa4b2307b6c3d49256b9b947d3450
Jeremy Clough <clough.jeremy@gmail.com>
2019-11-03 12:45:48 +0000

    Add percent function

--- tools/mf/load_tool.sh
+++ tools/mf/load_tool.sh
@@ -1,6 +1,31 @@
 TOOLS_DIR=$*
 
 # Description:
+#   Given a value (argument one), calculate the values +/- the percentage
+#   of the second value given (argument two).
+#
+# Usage:
+#   percent 500 5
+#
+function percent() {
+	if [ "$1" = '-h' ]; then
+		usage percent
+		return
+	fi
+
+	local  _pc=$(/usr/bin/bc <<<"$1*$2/100")
+        local _min=$(/usr/bin/bc <<<"$1-$_pc")
+        local _max=$(/usr/bin/bc <<<"$1+$_pc")
+
+ 	echo "In value: $1"
+        echo "In percent: $2"
+	echo "diff: $_pc"
+        echo "min: $_min"
+        echo "max: $_max"
+}
+
+
+# Description:
 #   Launch MF ED 32-bit command prompt.
 #
 # Usage:
